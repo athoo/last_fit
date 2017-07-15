9d7ea5510f37384e7e07d7b9b2ce16a03045d32d
yangpingjing <athoo@sina.com>
2017-07-15 15:29:39 -0500

    add bot

--- db_handler.js
+++ db_handler.js
@@ -13,24 +13,28 @@
 
 
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
 
-db.serialize(function() {
-  db.run("CREATE TABLE ClientId (ClientId varchar(255), AccessToken varchar(512), RefreshToken varchar(512), UpdateTime TIMESTAMP)");
-  console.log('second')
-  var stmt = db.prepare("INSERT INTO ClientId VALUES (?,?,?,?)",[],function(err, rows){
-    console.log('bad');
+var CreateTable = function(ClientId, AccessToken, RefreshToken, UpdateTime, Profile){
+  var db = new sqlite3.Database(':memory:');
+  db.serialize(function() {
+    db.run("CREATE TABLE Client (id INTEGER, AccessToken TEXT, RefreshToken TEXT, UpdateTime TIMESTAMP, Profile TEXT)");
+    var stmt = db.prepare("INSERT INTO Client VALUES (?,?,?,?,?)",[],function(err, rows){
+    });
+
+    stmt.run(ClientId, AccessToken, RefreshToken, UpdateTime, Profile);
+
+    stmt.finalize();
+
+    db.each("SELECT Profile FROM Client", function(err, row) {
+        console.log(row.Profile);
+    });
   });
-  console.log('first');
 
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i, 'this is an intro');
-  }
-  stmt.finalize();
+  db.close();
+}
 
-  db.each("SELECT rowid AS id, info, intro FROM ClientId", function(err, row) {
-      console.log(row.id + ": " + row.info + ":"+row.intro);
-  });
-});
-
-db.close();
+module.exports.CreateTable = CreateTable;
+//
+// var CreateTable = function(){
+//   var
+// }
--- index.js
+++ index.js
@@ -4,9 +4,22 @@ require('dotenv').config();
 const express = require('express')
 const request = require('request')
 const querystring = require('querystring')
+const db_handler = require('./db_handler')
+var moment = require('moment');
+const bodyParser = require('body-parser')
+
+
 var app = express();
+
+app.set('port', (process.env.PORT || 5000))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 var AUTH_INFO;
 var ACCESS_TOKEN;
+var REFRESH_TOKEN
+var USER_ID
+
 
 const code = {
   url: 'https://www.fitbit.com/oauth2/authorize',
@@ -33,28 +46,73 @@ const token = {
   }
 };
 
+let token = process.env.MESSENGER_TOKEN
+
+app.get('/webhook/', function(req, res) {
+	if(req.query['hub.verify_token'] === "blonde") {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send("Wrong token")
+})
+
+app.post('/webhook/', function(req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = messaging_events[0];
+		let sender = event.sender.id;
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			sendText(sender, "Text echo: " + text.substring(0, 100))
+		}
+	}
+	res.sendStatus(200)
+})
+
+
+function sendText(sender, text) {
+	let messageData = {text: text}
+	request({
+		url:"https://graph.facebook.com/v2.6/me/messages",
+		qs: {access_token: token},
+		method: "post",
+		json: {
+			recipient: {id: sender},
+			message: messageData
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log("sending error");
+		} else if (response.body.error) {
+			console.log("response body error")
+		}
+	})
+}
+
 // start login
 app.get('/', function(req, res){
   var code_url = code.url + '?' +querystring.stringify(code.qs)
-  console.log(code_url)
   res.redirect(code_url)
 })
 
 // return to the facebook messenger page
 app.get('/callback', function(req,res){
   token.form.code = querystring.parse(req.url)['/callback?code']
-  console.log(token);
   request.post(token, function(error, response, body) {
     AUTH_INFO = JSON.parse(body)
-    console.log(AUTH_INFO['access_token'])
+    // console.log(AUTH_INFO['access_token'])
     ACCESS_TOKEN = AUTH_INFO['access_token']
+    USER_ID = AUTH_INFO['user_id']
+    REFRESH_TOKEN = AUTH_INFO['refresh_token']
+    // console.log(AUTH_INFO)
     GetProfile(ACCESS_TOKEN, '-')
+    // console.log(GetProfile(ACCESS_TOKEN, '-'))
+    // res.send(GetProfile(ACCESS_TOKEN, '-'))
     res.redirect(process.env.MESSENGER_URL);
   });
 })
 
-app.listen(5000, function(){
-  console.log('the server is running on 5000');
+app.listen(app.get('port'), function(){
+  // console.log('the server is running on 5000');
 })
 
 var GetProfile = function(AccessToken, UserId){
@@ -66,7 +124,10 @@ var GetProfile = function(AccessToken, UserId){
   };
 
   request.get(profile, function(error, response, body) {
-    // console.log(typeof(body))
+
+    var Profile = JSON.parse(body)
+
+    db_handler.CreateTable(USER_ID,AccessToken,REFRESH_TOKEN,moment().format(),body)
     return JSON.parse(body)
   })
 
